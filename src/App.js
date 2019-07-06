986ec41457d2dfbbfa3e4f6e028b970764c14967
Will Adams <bushblade@gmail.com>
2019-07-06 16:02:09 +0100

    removed route components

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import React, { Component } from 'react'
 import NavBar from './components/NavBar/NavBar'
 import Result from './components/Result/Result'
 import Home from './components/Routes/Home'
-import Discover from './components/Routes/Discover'
-import NowPlaying from './components/Routes/NowPlaying'
-import TopRated from './components/Routes/TopRated'
 import Search from './components/Routes/Search'
+import SharedResults from './components/Routes/SharedResults'
+import { topRated, discover, nowPlaying } from './api'
 
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
@@ -35,9 +34,9 @@ class App extends Component {
         <div>
           <NavBar handleSubmit={this.handleSubmit} />
           <Route path="/" exact component={Home} />
-          <Route path="/discover" component={Discover} />
-          <Route path="/now-playing" component={NowPlaying} />
-          <Route path="/top-rated" component={TopRated} />
+          <Route path="/discover" component={() => <SharedResults url={discover} />} />
+          <Route path="/now-playing" component={() => <SharedResults url={nowPlaying} />} />
+          <Route path="/top-rated" component={() => <SharedResults url={topRated} />} />
           <Route path="/search" component={Search} />
 
           <Result results={this.state.results} />
