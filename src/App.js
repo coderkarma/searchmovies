f52277aaa8bd2fad866315e3e48467f82b2c95b6
Will Adams <bushblade@gmail.com>
2019-07-07 18:53:34 +0100

    cleanup and refactor to FC

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import NavBar from './components/NavBar/NavBar'
-import Result from './components/Result/Result'
 import Home from './components/Routes/Home'
 import Search from './components/Routes/Search'
 import SharedResults from './components/Routes/SharedResults'
@@ -8,20 +7,18 @@ import { topRated, discover, nowPlaying } from './api'
 
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          <NavBar handleSubmit={this.handleSubmit} />
-          <Route path="/" exact component={Home} />
-          <Route path="/discover" component={() => <SharedResults url={discover} />} />
-          <Route path="/now-playing" component={() => <SharedResults url={nowPlaying} />} />
-          <Route path="/top-rated" component={() => <SharedResults url={topRated} />} />
-          <Route path="/search/:movie" component={Search} />
-        </div>
-      </Router>
-    )
-  }
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <NavBar />
+        <Route path="/" exact component={Home} />
+        <Route path="/discover" component={() => <SharedResults url={discover} />} />
+        <Route path="/now-playing" component={() => <SharedResults url={nowPlaying} />} />
+        <Route path="/top-rated" component={() => <SharedResults url={topRated} />} />
+        <Route path="/search/:movie" component={Search} />
+      </div>
+    </Router>
+  )
 }
 export default App
