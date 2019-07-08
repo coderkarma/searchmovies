78737cc379f9cee1bca018d924bcfea46865813e
Will Adams <bushblade@gmail.com>
2019-07-08 18:45:35 +0100

    unused var

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,7 +4,6 @@ import { LinkContainer } from 'react-router-bootstrap'
 import SearchBar from './SearchBar'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
-import { Link } from 'react-router-dom'
 
 import '../Styles/NavBar.css'
 
