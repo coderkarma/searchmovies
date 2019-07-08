b8c5ebed113c832e411055de7fde8fd0c03d526d
Will Adams <bushblade@gmail.com>
2019-07-08 18:35:13 +0100

    added react-router-bootstrap for nav styling

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,45 +1,52 @@
-import React, { Component } from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
-import SearchBar from './SearchBar';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react'
+import { Navbar, Nav } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+import SearchBar from './SearchBar'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom'
 
-import '../Styles/NavBar.css';
+import '../Styles/NavBar.css'
 
 class NavBar extends Component {
-	render() {
-		return (
-			<React.Fragment>
-				<Navbar expand='lg' bg='dark' variant='dark' sticky='top'>
-					<Navbar.Brand href='/' />
-					<Navbar.Toggle aria-controls='basic-navbar-nav' />
-					<Navbar.Collapse id='basic-navbar-nav '>
-						<Nav className='mr-auto'>
-							<Nav>
-								<Link to='/'>
-									<FontAwesomeIcon icon={faCoffee} />
-								</Link>
-							</Nav>
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar expand="lg" bg="dark" variant="dark" sticky="top">
+          <Navbar.Brand href="/" />
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav ">
+            <Nav className="mr-auto">
+              <Nav.Item>
+                <LinkContainer to="/">
+                  <Nav.Link>
+                    <FontAwesomeIcon icon={faCoffee} />
+                  </Nav.Link>
+                </LinkContainer>
+              </Nav.Item>
 
-							<Nav>
-								<Link to='/discover'> Discover</Link>
-							</Nav>
+              <Nav.Item>
+                <LinkContainer to="/discover">
+                  <Nav.Link>Discover</Nav.Link>
+                </LinkContainer>
+              </Nav.Item>
 
-							<Nav>
-								<Link to='/now-playing'> Now Playing</Link>
-							</Nav>
-
-							<Nav>
-								<Link to='/top-rated'>Top Rated</Link>
-							</Nav>
-
-							<SearchBar handleSubmit={this.props.handleSubmit} />
-						</Nav>
-					</Navbar.Collapse>
-				</Navbar>
-			</React.Fragment>
-		);
-	}
+              <Nav.Item>
+                <LinkContainer to="/now-playing">
+                  <Nav.Link>Now Playing</Nav.Link>
+                </LinkContainer>
+              </Nav.Item>
+              <Nav.Item>
+                <LinkContainer to="/top-rated">
+                  <Nav.Link>Top Rated</Nav.Link>
+                </LinkContainer>
+              </Nav.Item>
+            </Nav>
+            <SearchBar handleSubmit={this.props.handleSubmit} />
+          </Navbar.Collapse>
+        </Navbar>
+      </React.Fragment>
+    )
+  }
 }
-export default NavBar;
+export default NavBar
