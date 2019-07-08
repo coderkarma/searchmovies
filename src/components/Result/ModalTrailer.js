a4e52b19d8deb908617b89d212433eadb6474d4a
Will Adams <bushblade@gmail.com>
2019-07-08 16:24:19 +0100

    close modal on outside click or escape key

--- src/components/Result/ModalTrailer.js
+++ src/components/Result/ModalTrailer.js
@@ -1,49 +1,49 @@
-import React, { useEffect, useState } from 'react';
-import { Modal, Button } from 'react-bootstrap';
-import YoutubeVideo from '../Result/YoutubeVideo';
-import '../Styles/ModalTrailer.css';
+import React, { useEffect, useState } from 'react'
+import { Modal, Button } from 'react-bootstrap'
+import YoutubeVideo from '../Result/YoutubeVideo'
+import '../Styles/ModalTrailer.css'
 
 const ModelTrailer = props => {
-	console.log('props ----', props);
-	const [ videoKey, setVideoKey ] = useState(undefined);
+  console.log('props ----', props)
+  const [videoKey, setVideoKey] = useState(undefined)
 
-	useEffect(() => {
-		const movieTrailerEndPoint = `http://api.themoviedb.org/3/movie/${props.movieId}?api_key=79ce19b11f80253ec95757f195144888&append_to_response=videos`;
+  useEffect(() => {
+    const movieTrailerEndPoint = `http://api.themoviedb.org/3/movie/${
+      props.movieId
+    }?api_key=79ce19b11f80253ec95757f195144888&append_to_response=videos`
 
-		fetch(movieTrailerEndPoint)
-			.then(response => response.json())
-			.then(movieTrailer => {
-				console.log('Here is the modal movie trailer', movieTrailer);
+    fetch(movieTrailerEndPoint)
+      .then(response => response.json())
+      .then(movieTrailer => {
+        console.log('Here is the modal movie trailer', movieTrailer)
 
-				const moviesTrailer = movieTrailer.videos.results[0].key;
+        const moviesTrailer = movieTrailer.videos.results[0].key
 
-				setVideoKey(moviesTrailer);
-			})
-			.catch(error => console.log("here's the error", error));
-	});
+        setVideoKey(moviesTrailer)
+      })
+      .catch(error => console.log("here's the error", error))
+  })
 
-	return (
-		<div>
-			<Modal
-				size='lg'
-				aria-labelledby='contained-modal-title-vcenter'
-				centered
-				show={true}
-			>
-				<Modal.Body>
-					<YoutubeVideo videoKey={videoKey} />
-				</Modal.Body>
-				<Modal.Footer>
-					<Button
-						className='close-button'
-						onClick={() => props.closeCallback()}
-					>
-						Close
-					</Button>
-				</Modal.Footer>
-			</Modal>
-		</div>
-	);
-};
+  return (
+    <div>
+      <Modal
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        show={true}
+        onHide={props.closeCallback}
+      >
+        <Modal.Body>
+          <YoutubeVideo videoKey={videoKey} />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button className="close-button" onClick={props.closeCallback}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  )
+}
 
-export default ModelTrailer;
+export default ModelTrailer
