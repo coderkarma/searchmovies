657cbdb8b8bddf4c1ebadea11a46bad54e2d62e7
coderkarma <karmadrukpa11@gmail.com>
2020-01-06 23:10:35 -0800

    fixed conflict code

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,49 +7,38 @@ import { Link } from 'react-router-dom';
 import '../Styles/NavBar.css';
 
 class NavBar extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <Navbar expand="lg" bg="dark" variant="dark" sticky="top">
-          <Navbar.Brand href="/" />
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav ">
-            <Nav className="mr-auto">
-              <Nav>
-                <Link to="/">
-                  <FontAwesomeIcon icon={faCoffee} />
-                </Link>
-              </Nav>
+    render() {
+        return (
+            <React.Fragment>
+                <Navbar expand="lg" bg="dark" variant="dark" sticky="top">
+                    <Navbar.Brand href="/" />
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav ">
+                        <Nav className="mr-auto">
+                            <Nav>
+                                <Link to="/">
+                                    <FontAwesomeIcon icon={faCoffee} />
+                                </Link>
+                            </Nav>
 
-              <Nav>
-                <Link to="/discover"> Discover</Link>
-              </Nav>
+                            <Nav>
+                                <Link to="/discover"> Discover</Link>
+                            </Nav>
 
-              <Nav>
-                <Link to="/now-playing"> Now Playing</Link>
-              </Nav>
+                            <Nav>
+                                <Link to="/now-playing"> Now Playing</Link>
+                            </Nav>
 
-              <Nav>
-                <Link to="/top-rated">Top Rated</Link>
-              </Nav>
+                            <Nav>
+                                <Link to="/top-rated">Top Rated</Link>
+                            </Nav>
 
-<<<<<<< HEAD
-              <SearchBar />
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-      </React.Fragment>
-    );
-  }
-=======
-							<SearchBar />
-							
-						</Nav>
-					</Navbar.Collapse>
-				</Navbar>
-			</React.Fragment>
-		);
-	}
->>>>>>> origin/feature/loading_spinner
+                            <SearchBar />
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </React.Fragment>
+        );
+    }
 }
 export default NavBar;
